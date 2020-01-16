bbddf7f094bf3b2533e802f7524d4be05e31bb3d
Andrew Chen <yongjhih@gmail.com>
2020-01-16 12:26:38 -0800

    Fix

--- providerx/lib/providerx.dart
+++ providerx/lib/providerx.dart
@@ -1,5 +1,8 @@
 library providerx;
 
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 extension ProviderBuildContextX<T extends BuildContext> on T {
   R provider<R extends ChangeNotifier>({bool listen}) => Provider.of<R>(this, listen: listen);
 }
