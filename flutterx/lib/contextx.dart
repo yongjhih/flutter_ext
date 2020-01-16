71d388cf9595abbcb99a343bc7ce8f77a24b54ea
Andrew Chen <yongjhih@gmail.com>
2020-01-16 12:15:33 -0800

    Add flutterx and providerx

---
+++ flutterx/lib/contextx.dart
@@ -0,0 +1,14 @@
+extension BuildContextX<T extends BuildContext> on T {
+  ThemeData theme({bool shadowThemeOnly}) => Theme.of(this, shadowThemeOnly: shadowThemeOnly);
+
+  NavigatorState navigator({
+    bool rootNavigator,
+    bool nullOk,
+  }) => Navigator.of(this,
+    rootNavigator: rootNavigator,
+    nullOk: nullOk,
+  );
+
+  FocusScopeNode focusScope() => FocusScope.of(this);
+}
+
---
+++ providerx/lib/providerx.dart
@@ -0,0 +1,3 @@
+extension ProviderBuildContextX<T extends BuildContext> on T {
+  R provider<R extends ChangeNotifier>({bool listen}) => Provider.of<R>(this, listen: listen);
+}
