7743340246447fe69feb2b2844978c52310397eb
Andrew Chen <yongjhih@gmail.com>
2020-01-17 12:44:34 -0800

    Remove as<R>

--- flutter_dtx/lib/flutter_dtx.dart
+++ flutter_dtx/lib/flutter_dtx.dart
@@ -1,6 +1,1 @@
 library flutter_dtx;
-
-// TODO: Move to dartx
-extension AsX<T extends Object> on T {
-  R as<R>() => this is R ? this as R : null;
-}
--- flutter_dtx/test/flutterx_test.dart
+++ flutter_dtx/test/flutterx_test.dart
@@ -3,11 +3,4 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_dtx/flutter_dtx.dart';
 
 void main() {
-  test('should as<T>()', () async {
-    const Object it = "Hello";
-    expect(it?.as<String>() ?? "", "Hello");
-    expect(it?.as<int>() ?? 0, 0);
-    const nullIt = null;
-    expect(nullIt?.as<String>() ?? "", "");
-  });
 }
