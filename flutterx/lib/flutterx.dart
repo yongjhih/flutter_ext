0e4e21fa279ede15754d8fa0e51a1ac2ed0cdf7b
Andrew Chen <yongjhih@gmail.com>
2020-01-16 22:55:01 -0800

    Remove unused code

--- flutterx/lib/flutterx.dart
+++ flutterx/lib/flutterx.dart
@@ -1,11 +1,5 @@
 library flutterx;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
-
 // TODO: Move to dartx
 extension AsX<T extends Object> on T {
   R as<R>() => this is R ? this as R : null;
