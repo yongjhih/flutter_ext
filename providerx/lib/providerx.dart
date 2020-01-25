53744afd735696b4fbb149dd2dd72d0ded8c51e8
Andrew Chen <yongjhih@gmail.com>
2020-01-24 21:52:33 -0800

    Add missing file

---
+++ providerx/lib/providerx.dart
@@ -0,0 +1,8 @@
+library providerx;
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+extension ProviderBuildContextX<T extends BuildContext> on T {
+  R provider<R extends ChangeNotifier>({bool listen = true}) => Provider.of<R>(this, listen: listen);
+}
