d2aa218a4b168423c2d54788f30b64ff8342b9cc
Andrew Chen <yongjhih@gmail.com>
2020-01-27 12:31:15 -0800

    Add Matcher.of(actual)

--- testx/lib/testx.dart
+++ testx/lib/testx.dart
@@ -2,69 +2,17 @@ library testx;
 
 import 'package:test/test.dart' as test;
 
-Expector<T> expects<T>(T actual) {
-  return Expector(actual);
-}
-
-class Expector<T> {
-  T actual;
-
-  Expector(T actual);
-
-  void isTrue() {
-    test.expect(actual, test.isTrue);
-  }
-  void isFalse() {
-    test.expect(actual, test.isFalse);
+extension MatcherX<T extends test.Matcher> on T {
+  void of<S>(S actual, {
+    String reason,
+    dynamic skip, // true or a String
+  }) {
+    test.expect(actual, this, reason: reason, skip: skip);
   }
 
-  void isNull() {
-    test.expect(actual, test.isNull);
+  bool match<S>(S actual, {
+    Map matchState,
+  }) {
+    return matches(actual, matchState);
   }
-
-  void isNotNull() {
-    test.expect(actual, test.isNotNull);
-  }
-
-  void isNotEmpty() {
-    test.expect(actual, test.isNotEmpty);
-  }
-
-  void isNaN() {
-    test.expect(actual, test.isNaN);
-  }
-
-  void same(T expected) {
-    test.expect(actual, test.same(expected));
-  }
-
-  void isA<S>() {
-    test.expect(actual, test.isA<S>());
-  }
-
-  void returnsNormally() {
-    test.expect(actual, test.returnsNormally);
-  }
-
-  void isMap() {
-    test.expect(actual, test.isMap);
-  }
-
-  void hasLength(test.Matcher matcher) {
-    test.expect(actual, test.hasLength(matcher));
-  }
-
-  void contains(expected) {
-    test.expect(actual, test.contains(expected));
-  }
-
-  void isIn(expected) {
-    test.expect(actual, test.isIn(expected));
-  }
-
-  void predicate<S>(bool f(S value),
-      [String description = 'satisfies function']) {
-    test.expect(actual, test.predicate<S>(f, description));
-  }
-
 }
--- testx/test/testx_test.dart
+++ testx/test/testx_test.dart
@@ -3,4 +3,12 @@ import 'package:test/test.dart';
 import 'package:testx/testx.dart';
 
 void main() {
+  test("MatcherX" , () {
+    isEmpty.of([]);
+    final list0 = [];
+    same(list0).of(list0);
+    //expect(isEmpty.match([]), isTrue);
+    //final list0 = [];
+    //expect(same(list0).match(list0), isTrue);
+  });
 }
