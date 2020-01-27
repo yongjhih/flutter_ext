e89b98000721cc83de2a66716bb47a41df9cec64
Andrew Chen <yongjhih@gmail.com>
2020-01-27 11:08:16 -0800

    Add Iterable.toStream

--- streamx/lib/streamx.dart
+++ streamx/lib/streamx.dart
@@ -3,3 +3,7 @@ library streamx;
 extension StreamX<T> on Stream<T> {
   Stream<T> distinctBy<R>(R comsume(T it)) => distinct((prev, next) => comsume(prev) == comsume(next));
 }
+
+extension IterableStreamX<T> on Iterable<T> {
+  Stream<T> toStream() => Stream.fromIterable(this);
+}
