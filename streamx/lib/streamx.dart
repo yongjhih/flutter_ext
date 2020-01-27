2860d8a79122e89c224ab09a2179f5d5891fb80e
Andrew Chen <yongjhih@gmail.com>
2020-01-27 11:23:03 -0800

    Add Stream.firstOrNull

--- streamx/lib/streamx.dart
+++ streamx/lib/streamx.dart
@@ -1,7 +1,11 @@
 library streamx;
 
 extension StreamX<T> on Stream<T> {
-  Stream<T> distinctBy<R>(R comsume(T it)) => distinct((prev, next) => comsume(prev) == comsume(next));
+  Stream<T> distinctBy<R>(R comsume(T it)) =>
+    distinct((prev, next) => comsume(prev) == comsume(next));
+
+  Future<T> firstOrNull() async =>
+    firstWhere((it) => true, orElse: () => null);
 }
 
 extension IterableStreamX<T> on Iterable<T> {
