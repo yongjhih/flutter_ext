c6c2742130a5e8a64b5249581bf74950a922e1cc
Andrew Chen <yongjhih@gmail.com>
2020-02-06 15:14:26 -0800

    Add context.consumer<>

--- providerx/lib/providerx.dart
+++ providerx/lib/providerx.dart
@@ -5,4 +5,5 @@ import 'package:provider/provider.dart';
 
 extension ProviderBuildContextX<T extends BuildContext> on T {
   R provider<R extends ChangeNotifier>({bool listen = true}) => Provider.of<R>(this, listen: listen);
+  R consumer<R extends ChangeNotifier>() => provider(listen: false);
 }
