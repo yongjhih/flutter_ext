b7d078193e29c0de69be6ee2a7482963ef9a1b6f
Andrew Chen <yongjhih@gmail.com>
2020-02-13 12:09:05 -0800

    Add Callable.asStream and Future.asStream

--- streamx/lib/streamx.dart
+++ streamx/lib/streamx.dart
@@ -1,5 +1,7 @@
 library streamx;
 
+import 'dart:async';
+
 extension StreamX<T> on Stream<T> {
   Stream<T> distinctBy<R>(R comsume(T it)) =>
     distinct((prev, next) => comsume(prev) == comsume(next));
@@ -10,4 +12,69 @@ extension StreamX<T> on Stream<T> {
 
 extension IterableStreamX<T> on Iterable<T> {
   Stream<T> asStream() => Stream.fromIterable(this);
+}
+
+extension IterableFutureStreamX<T> on Iterable<Future<T>> {
+  Stream<T> asStream() => Stream.fromFutures(this);
+}
+
+extension FutureStreamX<T> on Future<T> {
+  Stream<T> asStream() => Stream.fromFuture(this);
+}
+
+typedef _Callable<R> = R Function();
+
+extension CallableStreamX<T> on _Callable<T> {
+  Stream<T> asStream() => _CallableStream(this);
+}
+
+class _CallableStream<T> extends Stream<T> {
+  _CallableStream(_Callable<T> callable)
+      : _factory = (() {
+          try {
+            return Stream.value(callable());
+          } catch (e) {
+            return Stream.error(e);
+          }
+        });
+
+  final Stream<T> Function() _factory;
+
+  @override
+  bool get isBroadcast => true;
+
+  @override
+  StreamSubscription<T> listen(void onData(T event),
+          {Function onError, void onDone(), bool cancelOnError}) =>
+      _factory().listen(onData,
+          onError: onError, onDone: onDone, cancelOnError: cancelOnError);
+}
+
+typedef _FutureCallable<R> = Future<R> Function();
+
+extension FutureCallableStreamX<T> on _FutureCallable<T> {
+  Stream<T> asStream() => _FutureCallableStream(this);
+}
+
+class _FutureCallableStream<T> extends Stream<T> {
+  _FutureCallableStream(_FutureCallable<T> callable)
+      : _factory = (() {
+          try {
+            return Stream.fromFuture(callable());
+          } catch (e) {
+            return Stream.error(e);
+          }
+        });
+
+  final Stream<T> Function() _factory;
+
+  @override
+  bool get isBroadcast => true;
+
+
+  @override
+  StreamSubscription<T> listen(void onData(T event),
+          {Function onError, void onDone(), bool cancelOnError}) =>
+      _factory().listen(onData,
+          onError: onError, onDone: onDone, cancelOnError: cancelOnError);
 }
--- streamx/test/streamx_test.dart
+++ streamx/test/streamx_test.dart
@@ -9,4 +9,55 @@ void main() {
     expect(await Stream.fromIterable(["123", "abc"])
       .distinctBy((it) => it.length).last, "123");
   });
+
+  test('Callable.asStream', () async {
+    await expectLater(
+      (() => "hello").asStream(),
+      emitsInOrder(<dynamic>["hello", emitsDone]),
+    );
+    await expectLater(
+      (() => throw Exception()).asStream(),
+      emitsInOrder(<dynamic>[emitsError(isException), emitsDone]),
+    );
+  });
+
+  test('FutureCallable.asStream', () async {
+    await expectLater(
+      (() => Future.value("hello")).asStream(),
+      emitsInOrder(<dynamic>["hello", emitsDone]),
+    );
+    await expectLater(
+      (() => Future.error(Exception())).asStream(),
+      emitsInOrder(<dynamic>[emitsError(isException), emitsDone]),
+    );
+  });
+
+  test('Future.asStream', () async {
+    await expectLater(
+      (() async => "hello").asStream(),
+      emitsInOrder(<dynamic>["hello", emitsDone]),
+    );
+    await expectLater(
+      Future.value("hello").asStream(),
+      emitsInOrder(<dynamic>["hello", emitsDone]),
+    );
+    await expectLater(
+      Future.error(Exception()).asStream(),
+      emitsInOrder(<dynamic>[emitsError(isException), emitsDone]),
+    );
+  });
+
+  test('Futures.asStream', () async {
+    await expectLater(
+      [Future.value("hello"), Future.value("world")].asStream(),
+      emitsInOrder(<dynamic>["hello", "world", emitsDone]),
+    );
+  });
+
+  test('Iterable.asStream', () async {
+    await expectLater(
+      ["hello", "world"].asStream(),
+      emitsInOrder(<dynamic>["hello", "world", emitsDone]),
+    );
+  });
 }
